027263bf6b6017e08a5cb6e9463ac89a69ac6086
IT阿杰 <107783255+cosyjie@users.noreply.github.com>
2024-07-17 18:13:05 +0800

    -

--- menu.py
+++ menu.py
@@ -1,12 +1,10 @@
-from django.urls import reverse
-
 menu = {
     'module_database': {
         'child': [
             {
                 'name': 'db_mysql',
                 'title': 'MySQL数据库',
-                'href': reverse('module_database:db_mysql:index'),
+                'href': 'module_database:db_mysql:index',
             },
         ]
     }
